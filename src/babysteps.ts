9abf3ff69285566429262141b7e5f0ad6411bcbf
user <user@localhost>
2025-11-28 17:10:11 -0700

    R: extract fn printRemainingTimeCaption & more

--- src/babysteps.ts
+++ src/babysteps.ts
@@ -1,3 +1,5 @@
+export type TimeMinutesSeconds = { minute: number; second: number; }
+
 const BackgroundColorNeutral: string = "#ffffff";
 const BackgroundColorFailed: string = "#ffcccc";
 const BackgroundColorPassed: string = "#ccffcc";
@@ -14,98 +16,114 @@ let _threadTimer: NodeJS.Timeout;
 
 
 export function command(arg: string): void {
-    let args = { Url: { AbsoluteUri: `command://${arg}/` } }
-    console.log('called', arg, args.Url.AbsoluteUri);
-    if (args.Url.AbsoluteUri == "command://start/") {
-        document.body.innerHTML = CreateTimerHtml(getRemainingTimeCaption(0), BackgroundColorNeutral, true);
+  let args = { Url: { AbsoluteUri: `command://${arg}/` } }
+  console.log('called', arg, args.Url.AbsoluteUri);
+  if (args.Url.AbsoluteUri == "command://start/") {
+    document.body.innerHTML = CreateTimerHtml(printRemainingTimeCaption(getRemainingMinutesSeconds(0)), BackgroundColorNeutral, true);
 
-        _timerRunning = true;
-        _currentCycleStartTime = Date.now();
+    _timerRunning = true;
+    _currentCycleStartTime = Date.now();
 
-        _threadTimer = setInterval(function() {
-            if (_timerRunning) {
-                let elapsedTime: number = Date.now() - _currentCycleStartTime;
+    _threadTimer = setInterval(function () {
+      if (_timerRunning) {
+        let elapsedTime: number = Date.now() - _currentCycleStartTime;
 
-                if (elapsedTime >= SecondsInCycle * 1000 + 980) {
-                    _currentCycleStartTime = Date.now();
-                    elapsedTime = Date.now() - _currentCycleStartTime;
-                }
-                if (elapsedTime >= 5000 && elapsedTime < 6000 && _bodyBackgroundColor != BackgroundColorNeutral) {
-                    _bodyBackgroundColor = BackgroundColorNeutral;
-                }
+        if (elapsedTime >= SecondsInCycle * 1000 + 980) {
+          _currentCycleStartTime = Date.now();
+          elapsedTime = Date.now() - _currentCycleStartTime;
+        }
+        if (elapsedTime >= 5000 && elapsedTime < 6000 && _bodyBackgroundColor != BackgroundColorNeutral) {
+          _bodyBackgroundColor = BackgroundColorNeutral;
+        }
 
-                let remainingTime: string = getRemainingTimeCaption(elapsedTime);
-             
-                
-                if (_lastRemainingTime !== remainingTime) {
-                   
-                    if (remainingTime == "00:10") {
-                        playSound("2166__suburban-grilla__bowl-struck.wav");
-                    }
-                    else if (remainingTime == "00:00") {
-                        playSound("32304__acclivity__shipsbell.wav");
-                        _bodyBackgroundColor = BackgroundColorFailed;
-                    }
+        let remainingTime: string = printRemainingTimeCaption(getRemainingMinutesSeconds(elapsedTime));
 
-                    document.body.innerHTML = CreateTimerHtml(remainingTime, _bodyBackgroundColor, true);
-                    _lastRemainingTime = remainingTime;
-                }
-            }
-        }, 10);
-    }
-    else if (args.Url.AbsoluteUri == "command://stop/") {
-        _timerRunning = false;
-        clearInterval(_threadTimer)
-        document.body.innerHTML = CreateTimerHtml(getRemainingTimeCaption(0), BackgroundColorNeutral, false);
 
-    }
-    else if (args.Url.AbsoluteUri == "command://reset/") {
-        _currentCycleStartTime = Date.now();
-        _bodyBackgroundColor = BackgroundColorPassed;
-    }
-    else if (args.Url.AbsoluteUri == "command://quit/") {
-        document.body.innerHTML = "";
-        clearInterval(_threadTimer)
-    }
+        if (_lastRemainingTime !== remainingTime) {
+
+          if (remainingTime == "00:10") {
+            playSound("2166__suburban-grilla__bowl-struck.wav");
+          }
+          else if (remainingTime == "00:00") {
+            playSound("32304__acclivity__shipsbell.wav");
+            _bodyBackgroundColor = BackgroundColorFailed;
+          }
+
+          document.body.innerHTML = CreateTimerHtml(remainingTime, _bodyBackgroundColor, true);
+          _lastRemainingTime = remainingTime;
+        }
+      }
+    }, 10);
+  }
+  else if (args.Url.AbsoluteUri == "command://stop/") {
+    _timerRunning = false;
+    clearInterval(_threadTimer)
+    document.body.innerHTML = CreateTimerHtml(printRemainingTimeCaption(getRemainingMinutesSeconds(0)), BackgroundColorNeutral, false);
+
+  }
+  else if (args.Url.AbsoluteUri == "command://reset/") {
+    _currentCycleStartTime = Date.now();
+    _bodyBackgroundColor = BackgroundColorPassed;
+  }
+  else if (args.Url.AbsoluteUri == "command://quit/") {
+    document.body.innerHTML = "";
+    clearInterval(_threadTimer)
+  }
 
 };
 
 
 export function getRemainingTimeCaption(elapsedTime: number): string {
+  return printRemainingTimeCaption(getRemainingMinutesSeconds(elapsedTime))
+}
+//printRemainingTimeCaption(getRemainingMinutesSeconds(elapsedTime: number))
+export function printRemainingTimeCaption(
+  remaining: TimeMinutesSeconds
+): string {
+  return (
+    printZeroPrefixedNumber(remaining.minute) +
+    ":" +
+    printZeroPrefixedNumber(remaining.second)
+  );
+}
 
-    let remainingTime: Date = new Date((SecondsInCycle * 1000) - elapsedTime);
-    var minute: string | number = remainingTime.getMinutes();
-    var second: string | number = remainingTime.getSeconds();
-    if (minute < 10) { minute = '0' + minute; }
-    if (second < 10) { second = '0' + second; }
+function getRemainingMinutesSeconds(elapsedTime: number) {
+  let remainingTime: Date = new Date(SecondsInCycle * 1000 - elapsedTime);
+  return {
+    minute: remainingTime.getMinutes(),
+    second: remainingTime.getSeconds(),
+  };
+}
 
-    return '' + minute + ':' + second
+function printZeroPrefixedNumber(number: number): string {
+  if (number < 10) { return `0${number}`; }
+  return `${number}`;
 }
 
 export function CreateTimerHtml(timerText: string, bodyColor: string, running: boolean): string {
 
-    let timerHtml: string = "<div style=\"border: 3px solid #555555; background: " + bodyColor +
-        "; margin: 0; padding: 0;\">" +
-        "<h1 style=\"text-align: center; font-size: 30px; color: #333333;\">" + timerText +
-        "</h1>" +
-        "<div style=\"text-align: center\">";
-    if (running) {
-        timerHtml += "<a style=\"color: #555555;\" href=\"javascript:command('stop');\">Stop</a> " +
-            "<a style=\"color: #555555;\" href=\"javascript:command('reset');\">Reset</a> ";
-    }
-    else {
-        timerHtml += "<a style=\"color: #555555;\" href=\"javascript:command('start');\">Start</a> ";
-    }
-    timerHtml += "<a style=\"color: #555555;\" href=\"javascript:command('quit');\">Quit</a> ";
-    timerHtml += "</div></div>"
-    return timerHtml;
+  let timerHtml: string = "<div style=\"border: 3px solid #555555; background: " + bodyColor +
+    "; margin: 0; padding: 0;\">" +
+    "<h1 style=\"text-align: center; font-size: 30px; color: #333333;\">" + timerText +
+    "</h1>" +
+    "<div style=\"text-align: center\">";
+  if (running) {
+    timerHtml += "<a style=\"color: #555555;\" href=\"javascript:command('stop');\">Stop</a> " +
+      "<a style=\"color: #555555;\" href=\"javascript:command('reset');\">Reset</a> ";
+  }
+  else {
+    timerHtml += "<a style=\"color: #555555;\" href=\"javascript:command('start');\">Start</a> ";
+  }
+  timerHtml += "<a style=\"color: #555555;\" href=\"javascript:command('quit');\">Quit</a> ";
+  timerHtml += "</div></div>"
+  return timerHtml;
 
 }
 
 function playSound(url: string): void {
-    let audio = new Audio();
-    audio.src = `./src/sounds/${url}`;
-    console.log(audio.src);
-    audio.load();
-    audio.play();
+  let audio = new Audio();
+  audio.src = `./src/sounds/${url}`;
+  console.log(audio.src);
+  audio.load();
+  audio.play();
 }
--- test/babysteps.test.ts
+++ test/babysteps.test.ts
@@ -1,14 +1,28 @@
-import { describe, it } from "node:test"
-import assert from "node:assert"
+import { describe, it } from "node:test";
+import assert from "node:assert";
 
-import {CreateTimerHtml} from "../src/babysteps"
-describe("A new babysteps timer", () => {  
-    it("is successfully created",  () => {
-        var result  = CreateTimerHtml("Aap", "Noot", true)
-        var expected = "<div style=\"border: 3px solid #555555; background: Noot; margin: 0; padding: 0;\"><h1 style=\"text-align: center; font-size: 30px; color: #333333;\">Aap</h1><div style=\"text-align: center\"><a style=\"color: #555555;\" href=\"javascript:command('stop');\">Stop</a> <a style=\"color: #555555;\" href=\"javascript:command('reset');\">Reset</a> <a style=\"color: #555555;\" href=\"javascript:command('quit');\">Quit</a> </div></div>"
-        assert.equal(expected, result)
-        //expect(false).to.be.false 
-        //expect(30).to.equal(30)
-        //expect([]).to.be.empty;
-    })
-})
+import {
+  type TimeMinutesSeconds,
+  printRemainingTimeCaption,
+  CreateTimerHtml,
+} from "../src/babysteps";
+
+describe("A new babysteps timer", () => {
+  it("is successfully created", () => {
+    var result = CreateTimerHtml("Aap", "Noot", true);
+    var expected =
+      '<div style="border: 3px solid #555555; background: Noot; margin: 0; padding: 0;"><h1 style="text-align: center; font-size: 30px; color: #333333;">Aap</h1><div style="text-align: center"><a style="color: #555555;" href="javascript:command(\'stop\');">Stop</a> <a style="color: #555555;" href="javascript:command(\'reset\');">Reset</a> <a style="color: #555555;" href="javascript:command(\'quit\');">Quit</a> </div></div>';
+    assert.equal(expected, result);
+    //expect(false).to.be.false
+    //expect(30).to.equal(30)
+    //expect([]).to.be.empty;
+  });
+  it("prints the remaining time 01:02", () => {
+    var result = printRemainingTimeCaption({ minute: 1, second: 2 });
+    assert.equal("01:02", result);
+  });
+  it("prints the remaining time 11:22", () => {
+    var result = printRemainingTimeCaption({ minute: 11, second: 22 });
+    assert.equal("11:22", result);
+  });
+});
